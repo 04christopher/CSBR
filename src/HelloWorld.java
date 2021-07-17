00c3354d6d2c9337832c7fe5d4f83b931e634f99
FrozenHydrant <richard.you.777@gmail.com>
2021-07-16 22:40:19 -0700

    download

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,42 @@
-
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        try {
+            saveUrl("testing0.txt", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Rick_Astley_Dallas.jpg/220px-Rick_Astley_Dallas.jpg"); // downloads a very cool image to testing0.txt
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void saveUrl(final String filename, final String urlString)
+            throws MalformedURLException, IOException {
+        BufferedInputStream in = null;
+        FileOutputStream fout = null;
+        try {
+            in = new BufferedInputStream(new URL(urlString).openStream());
+            fout = new FileOutputStream(filename);
+
+            final byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (fout != null) {
+                fout.close();
+            }
+        }
     }
 }
