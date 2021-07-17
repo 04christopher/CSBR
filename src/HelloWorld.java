bd6836dac9a0de54bbd6b91b8313489002c22559
FrozenHydrant <richard.you.777@gmail.com>
2021-07-16 23:13:03 -0700

    cleaned code did some refactoring

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,8 @@ public class HelloWorld {
         System.out.println("Hello world!");
 
         try {
-            saveUrl("testing0.txt", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Rick_Astley_Dallas.jpg/220px-Rick_Astley_Dallas.jpg"); // downloads a very cool image to testing0.txt
+            saveUrl("testing01.png",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Rick_Astley_Dallas.jpg/220px-Rick_Astley_Dallas.jpg"); // downloads a very cool image to testing0.txt
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
@@ -19,23 +20,24 @@ public class HelloWorld {
 
     public static void saveUrl(final String filename, final String urlString)
             throws MalformedURLException, IOException {
-        BufferedInputStream in = null;
-        FileOutputStream fout = null;
+        BufferedInputStream inputStream = null;
+        FileOutputStream fileOut = null;
         try {
-            in = new BufferedInputStream(new URL(urlString).openStream());
-            fout = new FileOutputStream(filename);
+            inputStream = new BufferedInputStream(new URL(urlString).openStream());
+            fileOut = new FileOutputStream(filename);
 
             final byte data[] = new byte[1024];
-            int count;
-            while ((count = in.read(data, 0, 1024)) != -1) {
-                fout.write(data, 0, count);
+            int count = inputStream.read(data, 0, 1024);
+            while (count != -1) {
+                fileOut.write(data, 0, count);
+                count = inputStream.read(data, 0, 1024);
             }
         } finally {
-            if (in != null) {
-                in.close();
+            if (inputStream != null) {
+                inputStream.close();
             }
-            if (fout != null) {
-                fout.close();
+            if (fileOut != null) {
+                fileOut.close();
             }
         }
     }
